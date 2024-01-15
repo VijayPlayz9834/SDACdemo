ad978002388d0f0e9fb4d671f544661032b853d8
sdac-10 <sdac-10@sdac-10-PC>
2024-01-15 09:08:39 +0530

    w v1.2

--- SDACdemo/src/Whatsapp1.java
+++ SDACdemo/src/Whatsapp1.java
@@ -4,6 +4,7 @@ public class Whatsapp1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("whatsapp v1.1");
+          System.out.println("w v1.2");
 	}
 
 }
